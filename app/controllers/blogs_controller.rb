539cabaa9252c978a17726d48df41b604863f2ac
ymmtd0x0b <ymmtd0x0b@gmail.com>
2022-10-12 02:05:04 +0900

    通常ユーザーがアイキャッチを設定できる不具合の修正

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -4,6 +4,7 @@ class BlogsController < ApplicationController
   skip_before_action :authenticate_user!, only: %i[index show]
 
   before_action :set_blog, only: %i[show edit update destroy]
+  before_action :verify_access_right_of_blog, only: %i[show]
   before_action :verify_blog_owner, only: %i[edit update destroy]
 
   def index
@@ -49,10 +50,16 @@ class BlogsController < ApplicationController
   end
 
   def blog_params
-    params.require(:blog).permit(:title, :content, :secret, :random_eyecatch)
+    filter_list = %i[title content secret]
+    filter_list.push(:random_eyecatch) if current_user.premium
+    params.require(:blog).permit(filter_list)
   end
 
   def verify_blog_owner
     raise ActiveRecord::RecordNotFound, '該当ブログが見つかりませんでした' unless @blog.owned_by?(current_user)
+  end
+
+  def verify_access_right_of_blog
+    raise ActiveRecord::RecordNotFound, '該当ブログが見つかりませんでした' unless verify_blog_owner || !@blog.secret
   end
 end
